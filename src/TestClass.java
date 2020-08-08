e333a6b40db5f9e526451696d4bc06363e52817e
kadir-git <69384728+kadir-git@users.noreply.github.com>
2020-08-08 13:08:42 +0200

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,5 +3,6 @@ import java.util.Arrays;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba!");
+        System.out.println("GitHub dan merhaba!");
     }
 }
